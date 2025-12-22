8c2da3fbb2e068b820fc911f4ad47ddb0aead2e9
Maria Camila Pedraza Casasbuenas <36695574+camilapedraza@users.noreply.github.com>
2025-12-22 20:09:52 +0100

    fixed read/write errors in parse function

--- ps_parse_input.c
+++ ps_parse_input.c
@@ -6,7 +6,7 @@
 /*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 14:50:59 by mpedraza          #+#    #+#             */
-/*   Updated: 2025/12/22 19:17:49 by mpedraza         ###   ########.fr       */
+/*   Updated: 2025/12/22 20:07:36 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,10 @@ char	*parse_input(int n, char **argv)
 	index = 1;
 	while (index < n)
 	{
-		ft_strlcat(string, " ", ft_strlen(string) + 2);
 		size = ft_strlen(string) + ft_strlen(argv[index]);
 		ft_strlcat(string, argv[index], size + 1);
+		if (index < n - 1)
+			ft_strlcat(string, " ", ft_strlen(string) + 2);
 		index++;
 	}
 	return (string);
